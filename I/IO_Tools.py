4b4585158b2f2fd248f360e2436799f4c8b73328
Victor Mwenda <122853875+Victorkeemathie@users.noreply.github.com>
2023-06-30 15:10:04 +0300

    Create IO_Tools.py
    In this code, Pandas provides I/O tools for reading and writing various file formats. The examples demonstrate reading and writing CSV, Excel, and JSON files using functions like pd.read_csv(), df.to_csv(), pd.read_excel(), df.to_excel(), pd.read_json(), and df.to_json(). These functions enable seamless data exchange between Pandas DataFrames and external files, facilitating data analysis and storage

---
+++ I/IO_Tools.py
@@ -0,0 +1,101 @@
+# I/O Tools in Pandas Python:
+
+# 1. Reading & Writing CSV:
+# Pandas provides functions to read and write CSV (Comma-Separated Values) files.
+# The pd.read_csv() function is used to read CSV files into a DataFrame.
+# The df.to_csv() function is used to write a DataFrame to a CSV file
+# Example:
+
+import pandas as pd
+
+# Read CSV file from the specified location
+df = pd.read_csv('G:/Pandas/sample_data.csv')
+
+# Display the DataFrame
+print(df)
+
+# Example 2:
+
+import pandas as pd
+
+# Create a DataFrame
+data = {'Column1': [1, 2, 3],
+        'Column2': ['A', 'B', 'C']}
+df = pd.DataFrame(data)
+
+# Write DataFrame to CSV file at the specified location
+df.to_csv('G:/Pandas/output.csv', index=False)
+
+
+# 2. Reading & Writing Excel:
+# Pandas supports reading and writing Excel files.
+# The pd.read_excel() function is used to read Excel files into a DataFrame.
+# The df.to_excel() function is used to write a DataFrame to an Excel file
+# Example:
+
+import pandas as pd
+
+# Read Excel file from the specified location and sheet name
+df = pd.read_excel('G:/Pandas/sample_data.xlsx', sheet_name='Sheet1')
+
+# Display the DataFrame
+print(df)
+
+# Example 4:
+
+import pandas as pd
+
+# Create a DataFrame
+data = {'Column1': [1, 2, 3],
+        'Column2': ['A', 'B', 'C']}
+df = pd.DataFrame(data)
+
+# Write DataFrame to Excel file at the specified location with sheet name
+df.to_excel('G:/Pandas/output.xlsx', sheet_name='Sheet1', index=False)
+
+
+# Example:
+
+import pandas as pd
+
+# Create a DataFrame
+data = {'Name': ['John', 'Jane', 'Alice', 'Bob'],
+        'Age': [25, 28, 30, 35],
+        'City': ['New York', 'London', 'Paris', 'Tokyo']}
+df = pd.DataFrame(data)
+
+# Create an Excel writer object
+writer = pd.ExcelWriter('G:/Pandas/output.xlsx', engine='openpyxl')
+
+# Write the DataFrame to the Excel file
+df.to_excel(writer, sheet_name='Sheet1', index=False)
+
+# Save the Excel file
+writer._save()
+
+# Reading & Writing JSON:
+# Pandas allows reading and writing data in JSON (JavaScript Object Notation) format.
+# The pd.read_json() function is used to read JSON data into a DataFrame.
+# The df.to_json() function is used to write a DataFrame to a JSON file
+# Example:
+
+import pandas as pd
+
+# Read JSON file from the specified location
+df = pd.read_json('G:/Pandas/sample_data.json')
+
+# Display the DataFrame
+print(df)
+
+
+# Example 6:
+
+import pandas as pd
+
+# Create a DataFrame
+data = {'Column1': [1, 2, 3],
+        'Column2': ['A', 'B', 'C']}
+df = pd.DataFrame(data)
+
+# Write DataFrame to JSON file at the specified location
+df.to_json('G:/Pandas/output.json', orient='records')
